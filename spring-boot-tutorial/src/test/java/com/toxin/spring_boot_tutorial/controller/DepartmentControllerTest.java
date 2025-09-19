13a73af1d4147ddd53f282ff9502938a999b7485
Toxin <nishantkarpe1@gmail.com>
2025-09-20 02:58:43 +0530

    Controller test mocked

---
+++ spring-boot-tutorial/src/test/java/com/toxin/spring_boot_tutorial/controller/DepartmentControllerTest.java
@@ -0,0 +1,66 @@
+package com.toxin.spring_boot_tutorial.controller;
+
+import com.toxin.spring_boot_tutorial.entity.Department;
+import com.toxin.spring_boot_tutorial.service.DepartmentService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(DepartmentController.class)
+class DepartmentControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private DepartmentService departmentService;
+
+
+    private Department department;
+
+    @BeforeEach
+    void setUp() {
+        department = Department.builder().
+                departmentName("IT").
+                departmentAddress("Mumbai").
+                departmentCode("IT-08").
+                departmentId(1L).
+                build();
+
+
+    }
+
+    @Test
+    void saveDepartment() throws Exception {
+       Department inputDepartment = Department.builder().
+               departmentName("IT").
+               departmentAddress("Mumbai").
+               departmentCode("IT-08").
+               build();
+
+       Mockito.when(departmentService.saveDepartment(inputDepartment)).
+                thenReturn(department);
+
+       mockMvc.perform(post("/departments").
+                contentType(MediaType.APPLICATION_JSON).
+                content("{\n" +
+                        " \"departmentName\": \"IT\",\n" +
+                        " \"departmentAddress\": \"Mumbai\",\n" +
+                        " \"departmentCode\": \"IT-08\"\n" +
+                        "}" )).
+                andExpect(status().isOk());
+
+    }
+
+    @Test
+    void fetchDepartmentByID() {
+    }
+}
